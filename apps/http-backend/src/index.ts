176d6c633822f0da9671b615943fea6744b01515
sanjitxdutta <duttasanjit691@gmail.com>
2025-09-09 13:03:22 +0530

    created the signup, signin, create room endpoint along with gate the websocket server using token

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,19 @@
 import express from "express";
+import { middleware } from "./middleware";
 
 const app = express();
+
+app.post("/signup", (req, res) => {
+
+})
+
+app.post("/signin", (req, res) => {
+  
+})
+
+app.post("/room", middleware, (req, res) => {
+  
+})
 
 app.listen(4000, () => {
   console.log("Server running on http://localhost:4000");
