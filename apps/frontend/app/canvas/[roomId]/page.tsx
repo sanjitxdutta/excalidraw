0ec8fcfbebd0498d9a8ad9dc89378fe48257401e
sanjitxdutta <duttasanjit691@gmail.com>
2025-11-13 16:07:05 +0530

    Canvas Logic: Added basic rectangle.

--- apps/frontend/app/canvas/[roomId]/page.tsx
+++ apps/frontend/app/canvas/[roomId]/page.tsx
@@ -1,5 +1,47 @@
-export default function Canvas(){
+"use client";
+
+import { useEffect, useRef } from "react"
+
+export default function Canvas() {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        if (canvasRef.current) {
+            const canvas = canvasRef.current;
+            const ctx = canvas.getContext("2d");
+
+            if (!ctx) {
+                return;
+            }
+
+            let clicked = false;
+            let startX = 0;
+            let startY = 0;
+
+            canvas.addEventListener("mousedown", (e) => {
+                clicked = true;
+                startX = e.clientX;
+                startY = e.clientY;
+            })
+
+            canvas.addEventListener("mouseup", (e) => {
+                clicked = false;
+                // console.log(e.clientX);
+                // console.log(e.clientY);
+            })
+
+            canvas.addEventListener("mousemove", (e) => {
+                if (clicked) {
+                    const width = e.clientX - startX;
+                    const height = e.clientY - startY;
+                    ctx.clearRect(0, 0, canvas.width, canvas.height);
+                    ctx.strokeRect(startX, startY, width, height);
+                }
+            })
+        }
+    }, [canvasRef]);
+
     return <div>
-        Drawing Logic
+        <canvas ref={canvasRef} width={500} height={500} style={{ backgroundColor: "white" }}></canvas>
     </div>
 }
