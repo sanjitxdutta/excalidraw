9b10c54bf480e097f790c153f1e04802dbe0d30f
sanjitxdutta <duttasanjit691@gmail.com>
2025-11-13 17:48:00 +0530

    Added persistency for drawn shapes

--- apps/frontend/app/draw/useCanvasDraw.ts
+++ apps/frontend/app/draw/useCanvasDraw.ts
@@ -1,6 +1,23 @@
 import { useEffect, RefObject } from "react";
+
+type Shape = {
+    type: "rect";
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+} | {
+    type: "circle";
+    centerX: number;
+    centerY: number;
+    radius: number;
+}
+
 export default function useCanvasDraw(canvasRef: RefObject<HTMLCanvasElement | null>) {
-  useEffect(() => {
+ 
+    let existingShapes: Shape[] = [];
+
+    useEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
 
@@ -17,8 +34,18 @@ export default function useCanvasDraw(canvasRef: RefObject<HTMLCanvasElement | n
       startY = e.clientY;
     };
 
-    const handleMouseUp = () => {
+    const handleMouseUp = (e: MouseEvent) => {
       clicked = false;
+      const width = e.clientX - startX;
+      const height = e.clientY - startY;
+
+      existingShapes.push({
+        type: "rect",
+        x: startX,
+        y: startY,
+        width,
+        height,
+      });
     };
 
     const handleMouseMove = (e: MouseEvent) => {
@@ -27,9 +54,8 @@ export default function useCanvasDraw(canvasRef: RefObject<HTMLCanvasElement | n
       const width = e.clientX - startX;
       const height = e.clientY - startY;
 
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.fillStyle = "black";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      clearCanvas(existingShapes, canvas, ctx);
+
       ctx.strokeStyle = "white";
       ctx.strokeRect(startX, startY, width, height);
     };
@@ -44,4 +70,17 @@ export default function useCanvasDraw(canvasRef: RefObject<HTMLCanvasElement | n
       canvas.removeEventListener("mousemove", handleMouseMove);
     };
   }, [canvasRef]);
+}
+
+const clearCanvas = (existingShapes: Shape[], canvas: HTMLCanvasElement, ctx:CanvasRenderingContext2D) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height); 
+
+    existingShapes.map((shape) => {
+        if(shape.type === "rect"){
+            ctx.strokeStyle = "white";
+            ctx.strokeRect(shape.x, shape.y, shape.width, shape.height)
+        }
+    })
 }
